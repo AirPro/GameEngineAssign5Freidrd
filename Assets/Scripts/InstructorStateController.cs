346d526457185b6af35a0279e45fc8e2a8d84590
Bob Freid <17238974+AirPro@users.noreply.github.com>
2023-08-03 22:02:43 -0400

    Best Effort
    End of the day, One more try tomorrow.

--- Assets/Scripts/InstructorStateController.cs
+++ Assets/Scripts/InstructorStateController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class InstructorStateController : MonoBehaviour
 {
     // Creater an animator object
-    //Animator animator;
+    Animator animator;
     // Start is called before the first frame update
 
     public GameObject MainPlayer;
@@ -17,8 +17,8 @@ public class InstructorStateController : MonoBehaviour
 
     void Start()
     {
-        //animator = GetComponent<Animator>();
-        //Debug.Log(animator);
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
     }
 
     // Update is called once per frame
